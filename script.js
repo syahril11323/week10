c959d44f90b6cced114abd1bb48d44492bc4f5fa
syahril11323 <syahrilarf05@gmail.com>
2024-12-19 15:55:03 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let kursrupiah = 15831;
+
+function hitungkursrupiah(){
+let rupiah = document.getElementById("rupiah").Value;
+document.getElementById('dolllar').innerHTML = dolar / kursrupiah;
+}
+function hitungkursdollar(){
+let dollarNumber = document.getElementById("dollarNumber").Value;
+document.getElementById('rupiah').innerHTML = dolar / kursrupiah;
+}
